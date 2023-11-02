5d7395fe6c48f315f25811911b137321ae69e512
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-11-02 12:19:54 +0530

    reduce method command 1

---
+++ Array/arrMethod/index.js
@@ -0,0 +1,4 @@
+const str = "12345";
+const res = Array.from(str)
+console.log("res",res)
+console.log(str.split(""))
--- HOF/reduce/reduce.js
+++ HOF/reduce/reduce.js
@@ -1,16 +1,30 @@
-var input = ["a","b","c"]
-// Output: {0:a, 100:b, 200:c }
+const arr = [1,2,33,5,6,2];
+//find sum of arr element
+const output = arr.reduce(function(acc,curr){
+    acc= acc+curr;
+    return acc
+},0)
+console.log("output : ",output)
 
+// find max element from the arr
 
-var result = input.reduce(function(acc,current,index){
-    console.log(index,acc,current)
-    var obj = {}
-    var num =0;
-    // for(let i=0;i<input.length;i+100){
+function maxElement (arr){
+    var max = -Infinity;
+    for(let i=0;i<arr.length;i++){
+        if( arr[i]>max){
+            max =arr[i]
+        }
 
-    //     obj[current]=i;
-    //     console.log("obj", obj)
-    // }
-    
-})
-console.log("result",result)
+    }
+    return max
+}
+console.log("maxElement ", maxElement(arr))
+
+const maxOutput =  arr.reduce(function(max,curr){
+   
+ if(curr > max){
+    max=curr
+ }
+ return max
+},-Infinity)
+console.log("maxOutput :",maxOutput)
