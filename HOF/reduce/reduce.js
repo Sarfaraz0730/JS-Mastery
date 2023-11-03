3419df2af9151825904434a8a071e39c0483983c
Sarfaraz0730 <sarfarazpycoder@gmail.com>
2023-11-03 20:57:58 +0530

    reduce

--- HOF/reduce/reduce.js
+++ HOF/reduce/reduce.js
@@ -31,17 +31,43 @@ const maxOutput =  arr.reduce(function(max,curr){
 
 const apiData = [
  {firstName:"Sarfaraz",lastName:"Sheikh",age:23},
+ {firstName:"Aftab",lastName:"Sheikh",age:23},
  {firstName:"Ravi",lastName:"Verma",age:21},
  {firstName:"Ayush",lastName:"Singh",age:19},
+ {firstName:"Sharukh",lastName:"Singh",age:70},
 ]
 
+
+//  find all the firstName whose age is less than 30
+const ageLessThan30 = apiData.filter((item)=>item.age<30).map((name)=>name.firstName)
+console.log("ageLessThan30 ",ageLessThan30)
+// output{23:2, 21:1,19:1,70:1}
+
+
+//  const result = apiData.reduce((acc,curr,index)=>{
+    
+//     if(acc[curr.age]){
+//         acc[curr.age] = ++acc[curr.age]
+//     }else{
+//         acc[curr.age] =1
+//     }
+//     return acc
+
+//  },{})
+//  console.log("result",result)
+
 // output = ["Sarfaraz Sheikh", "Ravi Verma", "Ayush Singh"]
-const getFullname = (apiData)=>{
-    var ans = []
-  for(let i=0;i<apiData.length;i++){
-    console.log("fname",apiData[i].firstName)
-    ans.push(apiData[i].firstName  +" "+ apiData[i].lastName)
-  }
-  return ans
-}
-console.log("getFullname",getFullname(apiData))
+// const getFullname = (apiData)=>{
+//     var ans = []
+//   for(let i=0;i<apiData.length;i++){
+//     console.log("fname",apiData[i].firstName)
+//     ans.push(apiData[i].firstName  +" "+ apiData[i].lastName)
+//   }
+//   return ans
+// }
+// console.log("getFullname",getFullname(apiData))
+
+// const fullName = apiData.map((name)=>{
+//     return name.firstName + " " + name.lastName
+// })
+// console.log("fullName",fullName)
